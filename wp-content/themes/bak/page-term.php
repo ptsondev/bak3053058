c97a50cf3d34a68d955f5f5159b602ce32485a45
mac <mac@macs-MacBook-Pro.local>
2018-04-30 14:13:46 +0700

    terms

---
+++ wp-content/themes/bak/page-term.php
@@ -0,0 +1,75 @@
+<?php
+/*
+  Template Name: Term
+ */
+?>
+<?php get_header(); ?>
+
+<div id="main-body">
+    <div class="container main-wrapper">
+        <h1>Chính sách bán hàng</h1>
+        <div class="row">
+            <div id="terms" class="col-sm-9 col-xs-12">
+                <h3 class="block-title"><span class="step">1</span>Hình thức mua hàng</h3>
+                <div class="block-content">
+                    Có 2 hình thức mua hàng:
+                    <h4>1.1 Mua hàng trực tiếp <strong>(recommended)</strong></h4>     
+                    <div class="row">
+                        <div class="col-sm-2 col-sm-12"><img src="<?php echo PATH_TO_IMAGES?>/mua-hang-truc-tiep.png"></div>
+                        <div class="col-sm-10 col-sm-12">
+                            Bạn có thể đến showroom bếp An Khang tại 151 Tô Hiến Thành, Phường 13, Quận 10, TP HCM để xem trực tiếp sản phẩm, để chúng tôi có thể giúp bạn hiểu rõ hơn về các thông số như kích thước, công suất,.. của từng chiếc bếp. <br/>
+
+                            Chúng tôi cũng có sẵn 1 số màu sắc đa dạng để minh họa các mảng màu phổ biến của phòng bếp, qua đó để bạn có thể dễ dàng hình dung ra căn bếp nhà mình sau khi đưa sản phẩm mới vào. 
+                        </div>                    
+                    </div>
+                                        
+                    <h4 style="margin:20px 0;">1.2 Mua hàng online</h4>
+                    <div class="row" style="margin:5px 0 20px;">
+                        <div class="col-sm-2 col-sm-12"><img src="<?php echo PATH_TO_IMAGES?>/dat-hang-online.png"></div>
+                        <div class="col-sm-10 col-sm-12">
+                            Nếu bạn đã hoàn toàn hiểu rõ và ưng ý 1 chiếc bếp, bạn có thể đặt mua online.<br />
+                            Sau khi nhận được đơn hàng, nhân viên của chúng tôi sẽ liên lạc lại để xác nhận và tư vấn thêm. <br />
+                            Xem hướng dẫn tại <a href="">đây</a>
+                        </div>
+                    </div>
+                    
+                    <div class="important"><i class="fas fa-exclamation-circle"></i> Dù bạn chọn mua hàng qua hình thức nào thì bếp An Khang cũng sẽ luôn hỗ trợ & tư vấn nhiệt tình để bạn mua được chiếc bếp phù hợp. Bởi không gian bếp tiện nghi ấm cúng của gia đình bạn chính là mục tiêu hoạt động của chúng tôi.</div>
+                </div>
+                
+                <h3 class="block-title"><span class="step">2</span>Giao & Nhận hàng</h3>
+                <div class="row">                    
+                    <div class="col-sm-10 col-sm-12">
+                        <ul class="steps">
+                            <li>Sau khi đơn hàng được xác nhận, chúng tôi sẽ sắp xếp và báo trước ngày giờ giao hàng</li>
+                            <li>Khách hàng vui lòng báo trước tình trạng hiện tại của tủ bếp để chúng tôi lên kế hoạch triển khai.</li>
+                            <li>Nhân viên khi đến giao hàng sẽ hỗ trợ luôn phần lắp ráp sản phẩm</li>
+                            <li>Khách xác nhận trên phiếu giao hàng và thanh toán phần còn lại (nếu có)</li>
+                            <li class="important">Lưu ý: việc giao hàng và lắp ráp sản phẩm là hoàn toàn miễn phí</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-2 col-sm-12"><img src="<?php echo PATH_TO_IMAGES?>/giao-nhan-bep-an-khang.png"></div>
+                </div>
+                <h3 class="block-title"><span class="step">3</span>Chính sách bảo hành</h3>
+                <div class="row">                    
+                    <div class="col-sm-2 col-sm-12"><img src="<?php echo PATH_TO_IMAGES?>/cam-ket-bao-hanh.png"></div>
+                    <div class="col-sm-10 col-sm-12">
+                        <b>Bếp An Khang cam kết tuân thủ đúng & đầy đủ chính sách bảo hành:</b> <br />                        
+                            - Bảo hành 2 năm với các dòng bếp từ, bếp hồng ngoại. (Có thể lâu hơn với 1 số model đặc biệt)<br />
+                            - 1 đổi 1 trong 10 ngày nếu lỗi do nhà sản xuất<br />
+                        <b>Điều kiện bảo hành:</b>
+                            - Sản phẩm mua tại bếp An Khang, còn thời hạn bảo hành, và không có dấu hiệu hư hại do ngoại lực tác động<br/>    
+                    </div>                        
+                </div>        
+                <div class="important"><i class="fas fa-exclamation-circle"></i> Các dòng bếp từ cao cấp này có thời hạn sử dụng thực tế rất lâu, có thể lên đến 10 năm hoặc hơn. Do đó mặc dù đã hết thời hạn bảo hành, quý khách hàng cũng có thể liên hệ lại Bếp An Khang để sử dụng dịch vụ sửa chữa thiết bị của chúng tôi.</div>
+            </div>
+            
+            
+            <div class="col-sm-3 col-xs-12">
+                 <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
